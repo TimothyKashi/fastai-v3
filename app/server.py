5a75ba5133745b634567054c65f87ea29e92bbb6
Timothy Kashi <47909988+TimothyKashi@users.noreply.github.com>
2019-12-08 17:28:18 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/ftasj5beca0ee0e/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/ftasj5beca0ee0e/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
 classes = ['aeonium','donkeytail','jade','perlevonnurnberg','zebra']
