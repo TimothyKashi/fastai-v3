b70037e6e440d78f037e372b9f8130e2e4a6afd5
Timothy Kashi <timothyk5@yahoo.com>
2020-04-10 17:43:16 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -68,12 +68,16 @@ async def analyze(request):
 
 #test
 @app.route('/predict', methods=['POST'])
-async def analyze(request):
-    img_data = await request.form('file')
+async def predict(request):
+    img_data = await request.form('img')
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
     return JSONResponse({'result': str(prediction)})
+
+@app.route('/test', methods=['POST'])
+async def test(request):
+    return JSONResponse("its lit")
 
 
 if __name__ == '__main__':
